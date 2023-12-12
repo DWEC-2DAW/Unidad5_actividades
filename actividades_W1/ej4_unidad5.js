65a60bfa0cbcf0f57f347441e660b01e376a227f
davidlaekur <davidlaekur@me.com>
2023-12-12 10:39:35 +0100

    feat: repaso

--- actividades_W1/ej4_unidad5.js
+++ actividades_W1/ej4_unidad5.js
@@ -17,11 +17,13 @@ variable de parámetros en una función */
 function doublePipe(...functions) {
     return function (x) {
         let result = x;
-        for (let i = 0; i < functions.length; i++) {
-            result = functions[i](result);
-        }
-        return result;
+        for (let func of functions) {
+           for (let i = 0; i < 2; i++) {
+               result = func(result);
+           }
     }
+    return result;
+}
 }
 
 function double(x) {
@@ -37,3 +39,29 @@ console.log(multiplyPerFourAndAddSix(10));
 let addSixAndMultiplyPerFour = doublePipe(add3, double);
 
 console.log(addSixAndMultiplyPerFour(10));
+
+ // ejemplo mas sencillo para aclararme
+
+/* 
+
+function incrementar(x) { 
+    return x + 1; 
+}
+function duplicar(x) {
+    return x * 2; 
+}
+
+function doublePipe(...functions) {
+    return function (x) {
+        let result = x;
+        for (let func of functions) {
+            result = func(result);
+            result = func(result);
+        }
+        return result;
+    }
+}
+
+let ejemploFuncion = doublePipe(incrementar, duplicar);
+
+ */
